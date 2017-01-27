2810184f1977e2f85196a0e87e706f7abadab6e1
Roar Fredriksen <roarfred@gmail.com>
2017-01-27 19:31:58 +0100

    Created project (post from OledTemp)

---
+++ Displays/xns.display.small-mono-oled/xnsense.h
@@ -0,0 +1,19 @@
+// xnsense.h
+
+#ifndef _XNSENSE_h
+#define _XNSENSE_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+class xnsense
+{
+public:
+	static unsigned char bitmap[];
+};
+
+#endif
+
